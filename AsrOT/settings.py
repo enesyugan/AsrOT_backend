946127688f928fc98345df7e1360cc797a18ea3f
Doomse <ueqnn@student.kit.edu>
2022-02-23 17:31:32 +0100

    Add local settings for easier setting customization

--- AsrOT/settings.py
+++ AsrOT/settings.py
@@ -86,13 +86,6 @@ WSGI_APPLICATION = 'AsrOT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
@@ -140,3 +133,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Leave this as far down in this file as possible. Only settings that rely on definitions in the localsettings file should go below this import
+from .localsettings import *
